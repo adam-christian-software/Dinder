dc09a03d683c93940766693d754ecedc78d6c658
Jeremy Bader <jbader@conncoll.edu>
2015-05-25 17:29:28 -0400

    From right segue for matches

---
+++ testing swiping/FromRightSegue.h
@@ -0,0 +1,13 @@
+//
+//  FromRightSegue.h
+//  testing swiping
+//
+//  Created by jeremy bader on 5/25/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FromRightSegue : UIStoryboardSegue
+
+@end
---
+++ testing swiping/FromRightSegue.m
@@ -0,0 +1,34 @@
+//
+//  FromRightSegue.m
+//  testing swiping
+//
+//  Created by jeremy bader on 5/25/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import "FromRightSegue.h"
+#import "QuartzCore/QuartzCore.h"
+
+@implementation FromRightSegue
+
+-(void)perform {
+    
+    UIViewController *sourceViewController = (UIViewController*)[self sourceViewController];
+    UIViewController *destinationController = (UIViewController*)[self destinationViewController];
+    
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush; //kCATransitionMoveIn; //, kCATransitionPush, kCATransitionReveal, kCATransitionFade
+    transition.subtype = kCATransitionFromRight; //kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom
+    
+    
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+    
+    [sourceViewController.navigationController pushViewController:destinationController animated:NO];
+    
+}
+
+@end
