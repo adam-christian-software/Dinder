512c5e7c1cffe7a37d55ed5155230f1aee827391
Jeremy Bader <jbader@conncoll.edu>
2015-06-26 01:46:13 -0500

    Added simple table view controller, matches screen moving along

---
+++ testing swiping/SimpleTableAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleTableAppDelegate.h
+//  testing swiping
+//
+//  Created by jeremy bader on 6/26/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testing swiping/SimpleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTableViewController.h
+//  testing swiping
+//
+//  Created by jeremy bader on 6/26/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
