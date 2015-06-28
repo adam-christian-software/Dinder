9815e2717653f926d052a6d35705aef87b7bb609
Jeremy Bader <jbader@conncoll.edu>
2015-06-28 16:17:27 -0500

    Fixed unwind problem for sign up screen (had to add unwind segue to source)

--- testing swiping/LoginViewController.m
+++ testing swiping/LoginViewController.m
@@ -26,4 +26,34 @@
     // e.g. self.myOutlet = nil;
 }
 
+- (IBAction)unwindFromLeft:(UIStoryboardSegue*)sender
+{
+    UIViewController *sourceViewController = sender.sourceViewController;
+    
+    // Pull any data from the view controller which initiated the unwind segue.
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromLeft;
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+}
+
+- (IBAction)unwindFromRight:(UIStoryboardSegue*)sender
+{
+    UIViewController *sourceViewController = sender.sourceViewController;
+    
+    // Pull any data from the view controller which initiated the unwind segue.
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromRight;
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+}
+
 @end
